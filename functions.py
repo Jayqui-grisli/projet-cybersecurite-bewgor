6159674b696a5a7860725735624e6f055f8b0731
hugobaty <63297231+hugobaty@users.noreply.github.com>
2022-10-04 15:07:33 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,13 @@
+def traitement_date(file_name):
+    wordlist = []
+    return wordlist
+    
+
+def traitement_nomh(file_name):
+    wordlist = []
+    return wordlist
+
+
+def traitement_nombre(file_name):
+    wordlist = []
+    return wordlist
