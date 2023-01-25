141f567d5c09856dbb86afaef560d63363f3e715
Jayqui-grisli <zaouiisli@gmail.com>
2023-01-25 16:47:29 +0100

    crawler v1
    premiere version du crawler

--- web_crawler/crawler.py
+++ web_crawler/crawler.py
@@ -1,91 +1,101 @@
-# Python3 program for a word frequency
-# counter after crawling/scraping a web-page
 import requests
 from bs4 import BeautifulSoup
-import operator
-from collections import Counter
- 
-'''Function defining the web-crawler/core
-spider, which will fetch information from
-a given website, and push the contents to
-the second  function clean_wordlist()'''
- 
- 
-def start(url):
- 
-    # empty list to store the contents of
-    # the website fetched from our web-crawler
-    wordlist = []
-    source_code = requests.get(url).text
- 
-    # BeautifulSoup object which will
-    # ping the requested url for data
-    soup = BeautifulSoup(source_code, 'html.parser')
- 
-    # Text in given web-page is stored under
-    # the <div> tags with class <entry-content>
-    for each_text in soup.findAll('div', {'class': 'entry-content'}):
-        content = each_text.text
- 
-        # use split() to break the sentence into
-        # words and convert them into lowercase
-        words = content.lower().split()
- 
-        for each_word in words:
-            wordlist.append(each_word)
-        clean_wordlist(wordlist)
- 
-# Function removes any unwanted symbols
- 
- 
-def clean_wordlist(wordlist):
- 
-    clean_list = []
-    for word in wordlist:
-        symbols = "!@#$%^&*()_-+={[}]|\;:\"<>?/., "
- 
-        for i in range(len(symbols)):
-            word = word.replace(symbols[i], '')
- 
-        if len(word) > 0:
-            clean_list.append(word)
-    create_dictionary(clean_list)
- 
-# Creates a dictionary containing each word's
-# count and top_20 occurring words
- 
- 
-def create_dictionary(clean_list):
-    word_count = {}
- 
-    for word in clean_list:
-        if word in word_count:
-            word_count[word] += 1
+import re
+import itertools
+import os
+
+class scraper :
+    links_to_visit=[]
+    url=None
+    depth=0
+    myPath=os.path.realpath(os.path.dirname(__file__))+"\scraped_word_list.txt"
+
+    def __init__(self):
+        #demander l'url a l'user + traiter via regex si le format est bon
+        print("enter URL (format : example.something.else/stuff): ")
+        self.url = input()
+        regex=r"^((https://)?([A-Z]|[a-z])+\.([A-Z]|[a-z])+\.([A-Z]|[a-z])+/?)"
+        while (len(re.findall(regex,self.url))==0):
+            print("enter URL (format : example.something.else/stuff): ")
+            self.url = input()
+        if(re.search("^https://",self.url)==None):
+            self.url="https://"+self.url
+        
+        #demander a l'user la profonderur de recherche (limitée à 3)
+        print("enter depth of research (0-3) : ")
+        self.depth=int(input())
+        while (self.depth not in range(0,4)):
+            print("enter depth of research (0-3) : ")
+            self.depth=input()
+        
+        with open(self.myPath, "w", encoding="utf-16") as f:
+            f.write("")
+        #lancer la séquence de traitement
+        self.links_to_visit.append(self.url)
+        self.visitURL(self.depth,self.url)
+        
+
+
+    def visitURL(self,depth,url):
+        self.links_to_visit.remove(url)
+        try:
+            page=requests.get(url)
+            soup=BeautifulSoup(page.content,"html.parser")
+            self.parseSoup(soup)
+            print("visiting :",url)
+        except:
+            pass
+        if depth==0:
+            return
         else:
-            word_count[word] = 1
- 
-    ''' To get the count of each word in
-        the crawled page -->
- 
-    # operator.itemgetter() takes one
-    # parameter either 1(denotes keys)
-    # or 0 (denotes corresponding values)
- 
-    for key, value in sorted(word_count.items(),
-                    key = operator.itemgetter(1)):
-        print ("% s : % s " % (key, value))
- 
-    <-- '''
- 
-    c = Counter(word_count)
- 
-    # returns the most occurring elements
-    top = c.most_common(10)
-    print(top)
- 
- 
-# Driver code
-if __name__ == '__main__':
-    url = "https://www.geeksforgeeks.org/programming-language-choose/"
-    # starts crawling and prints output
-    start(url)
+            #rechercher toutes les urls de la page et les ajouter à la liste de urls à visiter
+            tags=soup.find_all("a")
+            for each_tag in tags:
+                try:
+                    each_url=each_tag["href"]
+                    #si l'url est relative on concatenne l'url parente pour recreer l'url
+                    if (re.search("^/.*",each_url)!=None):
+                        each_url=url+each_url
+                    #si la balise relie une ancre sur la page on ne l'ajoute pas
+                    if (re.search("^#",each_url)==None):
+                        self.links_to_visit.append(each_url)
+                except:
+                    pass
+        for each_unvisited in self.links_to_visit:
+            self.visitURL(depth-1,each_unvisited)
+
+
+    def parseSoup(self,soup):
+        word_list=[]
+        #je n'ai pas de meilleure solution pour tout extraire que de regarder dans les par et les div
+        div_html=soup.find_all("div")
+        p_html=soup.find_all("p")
+        for each_text in p_html:
+            temp_str=each_text.text
+            #supprimer les charactères spéciaux, les espaces et les mots de 1 à 2 lettres
+            for character in '‘’–.:/?,!-+*=()_|#"\';<>\n@':
+                temp_str=temp_str.replace(character," ")
+            temp_str=re.sub(r"(^ | +)"," ",temp_str)
+            temp_str=re.sub(r"\b\w{1,2}\b"," ",temp_str)
+            temp_list=temp_str.split()
+            word_list.append(temp_list)
+        for each_text in div_html:
+            temp_str=each_text.text
+            for character in '‘’–.:/?,!-+*=()_|#"\';<>\n@':
+                temp_str=temp_str.replace(character," ")
+            temp_str=re.sub(r" +"," ",temp_str)
+            temp_str=re.sub(r"\b\w{1,2}\b"," ",temp_str)
+            temp_list=temp_str.split()
+            word_list.append(temp_list)
+        #transformer list(list()) en list()
+        flat_word_list=list(itertools.chain(*word_list))
+        self.writeToFile(flat_word_list)
+
+    def writeToFile(self,words):
+        with open(self.myPath, "a", encoding="utf-16") as f:
+            for each_word in words:
+                f.write(each_word+"\n")
+
+            
+
+myscraper=scraper()
