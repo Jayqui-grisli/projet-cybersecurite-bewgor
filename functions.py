deddd8457305295b00dc87c72a98dd48433c23a7
hugobaty <63297231+hugobaty@users.noreply.github.com>
2023-01-28 23:42:03 +0100

    Update functions.py
    ajout fonctions

--- functions.py
+++ functions.py
@@ -24,3 +24,35 @@ def mixedUpper(dico):
         new_dico.append(inis)
     new_dico=h.merger(new_dico)
     return new_dico
+
+def num_to_month(num_list):
+    months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
+    month_list = []
+    for num in num_list:
+        if 1 <= num <= 12:
+            month_list.append(months[num-1])
+        else:
+            month_list.append(None)
+    return month_list
+
+
+def alternative_alphabet(dico, alphabet):
+    new_words = []
+    for word in dico:
+        new_word = ""
+        for char in word:
+            if char.isalpha():
+                new_word += alphabet[ord(char.lower())-97]
+            else:
+                new_word += char
+        new_words.append(new_word)
+    return new_words
+
+def insert_special_char(dico, special_chars):
+    new_words = []
+    for word in dico:
+        for special_char in special_chars:
+            for i in range(len(word) + 1):
+                new_word = word[:i] + special_char + word[i:]
+                new_words.append(new_word)
+    return new_words
