65813eea58de7d9b36b6949fe67e9360544e54b2
Grisli <zaouiisli@gmail.com>
2023-02-08 15:18:35 +0100

    Update crawler.py

--- web_crawler/crawler.py
+++ web_crawler/crawler.py
@@ -1,91 +1,48 @@
-# Python3 program for a word frequency
-# counter after crawling/scraping a web-page
+import logging
+from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
-import operator
-from collections import Counter
- 
-'''Function defining the web-crawler/core
-spider, which will fetch information from
-a given website, and push the contents to
-the second  function clean_wordlist()'''
- 
- 
-def start(url):
- 
-    # empty list to store the contents of
-    # the website fetched from our web-crawler
-    wordlist = []
-    source_code = requests.get(url).text
- 
-    # BeautifulSoup object which will
-    # ping the requested url for data
-    soup = BeautifulSoup(source_code, 'html.parser')
- 
-    # Text in given web-page is stored under
-    # the <div> tags with class <entry-content>
-    for each_text in soup.findAll('div', {'class': 'entry-content'}):
-        content = each_text.text
- 
-        # use split() to break the sentence into
-        # words and convert them into lowercase
-        words = content.lower().split()
- 
-        for each_word in words:
-            wordlist.append(each_word)
-        clean_wordlist(wordlist)
- 
-# Function removes any unwanted symbols
- 
- 
-def clean_wordlist(wordlist):
- 
-    clean_list = []
-    for word in wordlist:
-        symbols = "!@#$%^&*()_-+={[}]|\;:\"<>?/., "
- 
-        for i in range(len(symbols)):
-            word = word.replace(symbols[i], '')
- 
-        if len(word) > 0:
-            clean_list.append(word)
-    create_dictionary(clean_list)
- 
-# Creates a dictionary containing each word's
-# count and top_20 occurring words
- 
- 
-def create_dictionary(clean_list):
-    word_count = {}
- 
-    for word in clean_list:
-        if word in word_count:
-            word_count[word] += 1
-        else:
-            word_count[word] = 1
- 
-    ''' To get the count of each word in
-        the crawled page -->
- 
-    # operator.itemgetter() takes one
-    # parameter either 1(denotes keys)
-    # or 0 (denotes corresponding values)
- 
-    for key, value in sorted(word_count.items(),
-                    key = operator.itemgetter(1)):
-        print ("% s : % s " % (key, value))
- 
-    <-- '''
- 
-    c = Counter(word_count)
- 
-    # returns the most occurring elements
-    top = c.most_common(10)
-    print(top)
- 
- 
-# Driver code
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s:%(message)s',
+    level=logging.INFO)
+
+class Crawler:
+
+    def __init__(self, urls=[]):
+        self.visited_urls = []
+        self.urls_to_visit = urls
+
+    def download_url(self, url):
+        return requests.get(url).text
+
+    def get_linked_urls(self, url, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        for link in soup.find_all('a'):
+            path = link.get('href')
+            if path and path.startswith('/'):
+                path = urljoin(url, path)
+            yield path
+
+    def add_url_to_visit(self, url):
+        if url not in self.visited_urls and url not in self.urls_to_visit:
+            self.urls_to_visit.append(url)
+
+    def crawl(self, url):
+        html = self.download_url(url)
+        for url in self.get_linked_urls(url, html):
+            self.add_url_to_visit(url)
+
+    def run(self):
+        while self.urls_to_visit:
+            url = self.urls_to_visit.pop(0)
+            logging.info(f'Crawling: {url}')
+            try:
+                self.crawl(url)
+            except Exception:
+                logging.exception(f'Failed to crawl: {url}')
+            finally:
+                self.visited_urls.append(url)
+
 if __name__ == '__main__':
-    url = "https://www.geeksforgeeks.org/programming-language-choose/"
-    # starts crawling and prints output
-    start(url)
+    Crawler(urls=['https://www.imdb.com/']).run()
