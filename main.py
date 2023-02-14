3bc71b1437047ba52d3df35da42424ab4c25fd15
Jayqui-grisli <zaouiisli@gmail.com>
2023-02-14 20:18:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ do_skip=input("Voulez vous ne pas générer et récupérer les dictionnaires int
 if do_skip=="Y" or do_skip=="y":
     skip_gen=True
 
-if not sikp_gen :
+if not skip_gen :
     nom_sortie=input("Entrez le nom du fichier à générer \n")
     permut=input("Entrez le nombre de permuation (3 par défault)\n /!\ Au delà de 5, vous risquez de mourrir avant la fin de l'exéctution du programme /!\ \n")
     if nom_sortie=='':
